7178a45912067f56d30ab25357774f6f174819b7
MagicBarrel7887 <90459450+MagicBarrel7887@users.noreply.github.com>
2024-01-09 14:15:45 -0600

    Update engine.h
    Redundant blank line at the start of a code block should be deleted.' issue in include\engine.h

--- include/engine.h
+++ include/engine.h
@@ -1,24 +1,14 @@
 #pragma once
-
 #ifndef __ENGEN__H__
 #define __ENGEN__H__
-
-
 #define ttp229_statue false
 #define DeBUG false
-
-
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
 #include <Wire.h>
-
 #if ttp229_statue == true
-
 #include <TTP229.h>
-
 #endif
-
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
@@ -67,23 +57,12 @@ private:
 
     int DISP_Key;
     int FUNC_Key;
-
-    
-
     int FUNC_data[9];
     int LOCO;
-
     int EMERGENCY_STOP_Key;
     int error_number;
-
-   
-
-    
-
     int analogPin_joy = A3;
     int analog_joystick;
-    
-
     int but_1;
     int but_2;
     int but_3;
@@ -98,14 +77,12 @@ public:
 
    unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
    int e_db();
-
    int Function(/* args */);
    int Arduino_digital_analog_int(/* args */);
    int Loco_Selection(/* args */);
    int MU(/* args */);
    int Switch(/* args */);
    int Power(/* args */);
-
    int Status_Edit(/* args */);
    int Programming_Paged(/* args */);
    int Programming_Physical(/* args */);
@@ -115,34 +92,24 @@ public:
    int Find(/* args */);
    int display_8();
    int push_buttons_joystick();
-
    bool nrf24_error = false;
-
    int RX_ADD;
-
    int error;
-
    int display_Function = 0;
    int temp_code;
    int cv_code;
    int lts; // L Throttle speed
    int data_io;
-
    int L_Throttle;
    int R_Throttle;
-
    bool sitch;
    bool Powr;
-
    int lta; //L Throttle Address
    int rta; //R Throttle Address
-
    int analog_joystick_map;
-
    //** @brief code loop counter -- Average loops per second @return 1
    unsigned long loopCount;
    unsigned long startTime;
-   
    int get_count();
 
     #if ttp229_statue == true
@@ -181,17 +148,10 @@ int engen::Arduino_digital_analog_int(){
   pinMode(but_5_pin, INPUT_PULLUP);
   pinMode(but_6_pin, INPUT_PULLUP);
   pinMode(but_7_pin, INPUT_PULLUP);
-
-
   return 0;
 }
 //** @brief check buttons and Debounce @return 0 good, 1 error, 2 not at debouncing timer delay set
-
 int engen::push_buttons_joystick(){
-
-
-
-
    analog_joystick_x = analogRead(analogPin_joy_x);
    analog_joystick_map = analogRead(analogPin_joy);
 
@@ -201,8 +161,6 @@ int engen::push_buttons_joystick(){
    if(analog_joystick_map <= 0){
       analog_joystick_map = map(analog_joystick_map, -1023, 0, -255, 0);
    }
-
-
    but_1 = digitalRead(but_1_pin);
    but_2 = digitalRead(but_2_pin);
    but_3 = digitalRead(but_3_pin);
@@ -210,11 +168,7 @@ int engen::push_buttons_joystick(){
    but_5 = digitalRead(but_5_pin);
    return 0;
    }
-
-
-
 //** @brief train controller functions @return null
-
  int engen::Function(/* args */)
  {
     for (int i = 0; i < 9; ++i) {
@@ -223,7 +177,6 @@ int engen::push_buttons_joystick(){
 
     return 0;
  }
-
  int engen::Loco_Selection(/* args */)
  {
   // lcd.home();
@@ -231,7 +184,6 @@ int engen::push_buttons_joystick(){
   //  lcd.print(Lo);
     return 0;
  }
-
  int engen::MU(/* args */)
  {
   //  lcd.home();
@@ -239,7 +191,6 @@ int engen::push_buttons_joystick(){
   //  lcd.print(Mu);
     return 0;
  }
-
  int engen::Switch(/* args */)
  {
   //  lcd.home();
@@ -247,7 +198,6 @@ int engen::push_buttons_joystick(){
   // lcd.print(Sw);
     return 0;
  }
-
  int engen::Power(/* args */)
  {
   //  lcd.home();
@@ -255,46 +205,34 @@ int engen::push_buttons_joystick(){
   //  lcd.print(Pw);
     return 0;
  }
-
  int engen::Status_Edit(/* args */)
  {
     return 0;
  }
-
  int engen::Programming_Paged(/* args */)
  {
     return 0;
  }
-
  int engen::Programming_Physical(/* args */)
  {
     return 0;
  }
-
  int engen::Programming_Direct(/* args */)
  {
     return 0;
  }
-
  int engen::Programming_Operations_Mode(/* args */)
  {
     return 0;
  }
-
  int engen::Recall(/* args */)
  {
     return 0;
  }
-
  int engen::Find(/* args */)
  {
     return 0;
  }
-
-
-
-
-
 //** @brief code loop counter -- Average loops per second @return 1
  int engen::get_count()
  {
@@ -305,11 +243,8 @@ int engen::push_buttons_joystick(){
         startTime = millis();
         loopCount = 0;
     }
-
     return 1; // or some valid int
  }
-
-
 //** @brief display_8 defalt display mode @return int
  int engen::display_8()
  {
@@ -362,16 +297,12 @@ int engen::push_buttons_joystick(){
 
         display.display();
     }
-
         return 0;
  }
-
 //** @brief ttp229_statue defalt set to false ttp229 not use @return bool false
     #if ttp229_statue == true
-
         int engen::ttp229_get_states(){
         ttp229.readKeys();
-
         Serial.print("Key states: ");
         for (int i = 0; i < 16; ++i) {
         if (ttp229.isKeyPress(i)) {
@@ -382,82 +313,55 @@ int engen::push_buttons_joystick(){
      }
      return 0;
      }
-
      int engen::ttp229_get_Pressed(){
-
      ttp229.readKeys();
-
      int key = ttp229.getKey(); // gets Pressed key out put int
      Serial.print("Pressed key: ");
      Serial.print(key);
-
      Serial.print("   ");
      if (ttp229.isKeyDown(0)) {
-
         Serial.print("Key 0 is down");
      } else if (ttp229.isKeyUp(0)) {
-
         Serial.print("Key 0 is up");
      }
-
      return key;
      }
-
-
      int engen::ttp229_is_Key_Press(int key){
      ttp229.readKeys();
-
      // int keys = ttp229.getKey(); // gets Pressed key out put int
      int keys = ttp229.isKeyDown(key);
-
-
      return keys;
      }
-
-
      int engen::ttp229_get_any_Pressed(){
-
      ttp229.readKeys();
      bool gb;
      int key = ttp229.getKey(); // gets Pressed key out put int
      Serial.print("Pressed key: ");
      Serial.print(key);
-
      for(int g = 0; g < 16; g++){
      bool ga = ttp229.isKeyDown(g);
-
      if(!ga){
         gb = false;
          break;
        }
-
      if (ga) {
         gb = true;
      }
      }
-
-
      Serial.print("   ");
      if (ttp229.isKeyDown(0)) {
-
         Serial.print("Key 0 is down");
      } else if (ttp229.isKeyUp(0)) {
-
         Serial.print("Key 0 is up");
      }
-
         return gb;
      }
 #endif
-
  int engine_debug(int a, String b){
-
     const char *c = b.c_str();
-
     Serial.print(c);   // inputstring
     Serial.print(" ");   // space
     Serial.println(a); // payload was not delivered
-
     return 1;
  }
 
@@ -467,45 +371,29 @@ int engen::push_buttons_joystick(){
     engine_debug(R_Throttle, "R_Throttle");
     engine_debug(R_Reverse_Key, "R_Reverse_Key");
     engine_debug(L_Reverse_Key, "L_Reverse_Key");
-
     engine_debug(DISP_Key, "DISP_Key");
     engine_debug(FUNC_Key, "FUNC_Key");
-
     engine_debug(sitch, "sitch");
     engine_debug(Powr, "Powr");
-
     engine_debug(0, "FUNC_data unavalbel");
     engine_debug(LOCO, "LOCO");
-
     engine_debug(EMERGENCY_STOP_Key, "EMERGENCY_STOP_Key");
     engine_debug(error_number, "error_number");
-
     engine_debug(lta, "lta");
     engine_debug(lta, "rta");
-
     engine_debug(analog_joystick_map, "analog_joystick");
-
     engine_debug(but_1, "but_1");
     engine_debug(but_2, "but_2");
     engine_debug(but_3, "but_3");
     engine_debug(but_4, "but_4");
     engine_debug(but_5, "but_5");
-    
     engine_debug(error, "error");
-  
-
     return 1;
  }
-
  int db(int a, String b){
-
     const char *c = b.c_str();
-
     Serial.print(c); // inputstring
     Serial.println(a); // payload was not delivered
-
     return 1;
  }
-
-
 #endif //!__ENGEN__H__
